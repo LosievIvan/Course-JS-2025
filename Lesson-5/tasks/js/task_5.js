5cc19b8de8edf5c1ca8cc84f75596ad10abd360a
Losiev Ivan <96789531+LosievIvan@users.noreply.github.com>
2025-02-09 15:53:00 +0200

    Update task_5.js

--- Lesson-5/tasks/js/task_5.js
+++ Lesson-5/tasks/js/task_5.js
@@ -1,7 +1,8 @@
-let numberWeeks = parseFloat(prompt('Введіть кількість тижнів:', '1'))
-
-let totalSum = 0
-for (let weeks = 1; weeks <= numberWeeks; weeks++) {
+if (confirm('Почати тестування?')) {
+	let numberWeeks = parseFloat(prompt('Введіть кількість тижнів:', '1'))
+	
+	let totalSum = 0
+	for (let weeks = 1; weeks <= numberWeeks; weeks++) {
 	let totalWeeks = 0
 	for (let day = 1; day <= 7;) {
 		let numberWeeks = parseFloat(prompt(`Сума прибутку за день №${day++} тижня №${weeks}`, `1000`))
@@ -10,4 +11,3 @@ for (let weeks = 1; weeks <= numberWeeks; weeks++) {
 	document.write(`<p>Сума прибутку за тиждень №${weeks} = ${totalWeeks}</p>`)
 	totalSum+=totalWeeks
 }
-document.write(`<p>Сума прибутку за весь час = ${totalSum}</p>`)
